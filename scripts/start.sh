833ce6894130f480ac41e4198ed7aebd4e1895f9
dpvDuncan <dpvduncan@hotmail.fr>
2020-02-03 15:55:02 +0100

    Update user

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,14 +6,14 @@ USERNAME=$(getent passwd $PUID | cut -d: -f1)
 
 if [ ! $GROUPNAME ]
 then
-        addgroup -g $PGID medusa
-        GROUPNAME=medusa
+        addgroup -g $PGID guacamole
+        GROUPNAME=guacamole
 fi
 
 if [ ! $USERNAME ]
 then
-        adduser -G $GROUPNAME -u $PUID -D medusa
-        USERNAME=medusa
+        adduser -G $GROUPNAME -u $PUID -D guacamole
+        USERNAME=guacamole
 fi
 
 if [ ! ${BASE_URL} ]
